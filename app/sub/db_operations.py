e0dd25eb4d5ee567817176f761fadd0847c8e2f5
Yushi Kiyota <2020kiyu@users.noreply.github.com>
2024-07-21 20:03:07 +0900

    おねがい10

--- app/sub/db_operations.py
+++ app/sub/db_operations.py
@@ -14,7 +14,7 @@ supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # 全データ取得
 def get_all_users():
-    response = supabase.table('user_data').select('*').execute()
+    response = supabase.table('USER_DATA').select('*').execute()
     users = response.data  # データをusers変数に格納
     # id をキーにして XP と LEVEL を持つ辞書を作成
     user_dict = {}
@@ -25,20 +25,20 @@ def get_all_users():
 
 # 取得
 def get_user_data(user_id):
-    response = supabase.table('user_data').select('*').eq('id', user_id).execute()
+    response = supabase.table('USER_DATA').select('*').eq('id', user_id).execute()
     return response.data
 
 # 更新
 def update_user_data(user_id, xp, level):
-    response = supabase.table('user_data').update({'xp': xp, 'level': level}).eq('id', user_id).execute()
+    response = supabase.table('USER_DATA').update({'xp': xp, 'level': level}).eq('id', user_id).execute()
     return response.data
 
 # 挿入
 def insert_user_data(user_id, xp, level):
-    response = supabase.table('user_data').insert({'id': user_id, 'xp': xp, 'level': level}).execute()
+    response = supabase.table('USER_DATA').insert({'id': user_id, 'xp': xp, 'level': level}).execute()
     return response.data
 
 # ランキング取得
 def get_users_sorted_by_xp():
-    response = supabase.table('user_data').select('*').order('xp', desc=True).execute()
+    response = supabase.table('USER_DATA').select('*').order('xp', desc=True).execute()
     return response.data
