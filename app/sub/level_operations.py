638e986dd46d7acdc44d91c32d7132b0348b144a
Yushi Kiyota <2020kiyu@users.noreply.github.com>
2024-07-21 20:48:23 +0900

    おねがい45

--- app/sub/level_operations.py
+++ app/sub/level_operations.py
@@ -86,7 +86,8 @@ async def add_roles(member, next_level):
 
 # ユーザーのすべてのロールを削除する
 async def remove_all_roles(member):
-    roles = member.roles[1:]  # member.roles[0]は@everyoneロールなのでスキップ
-    if roles:
-        await member.remove_roles(*roles)
+    if member.roles and member.roles.length > 1:
+        roles = member.roles[1:]  # member.roles[0]は@everyoneロールなのでスキップ
+        if roles:
+            await member.remove_roles(*roles)
 
