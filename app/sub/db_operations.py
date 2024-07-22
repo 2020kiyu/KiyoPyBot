8e921c34f174bfe4fa0a50aea1b362657b98c373
Yushi Kiyota <2020kiyu@users.noreply.github.com>
2024-07-22 12:28:51 +0900

    Changes1228

--- app/sub/db_operations.py
+++ app/sub/db_operations.py
@@ -7,13 +7,14 @@ from dotenv import load_dotenv
 #############################
 # 変数
 #############################
-#環境変数の読み込み
+# 環境変数の読み込み
 load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 # DBの設定
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
 
 #############################
 # 処理
@@ -25,12 +26,14 @@ def get_all_users():
     # id をキーにして XP と LEVEL を持つ辞書を作成
     user_dict = {}
     for user in users:
-        user_dict[user['id']] = {'xp': user['xp'], 'level': user['level']}
+        user_dict[int(user['id'])] = {'xp': user['xp'], 'level': user['level']}
     # 辞書形式でデータを返却
     return user_dict
 
+
 # 取得
 def get_user_data(user_id):
+    user_id = str(user_id)
     response = supabase.table('USER_DATA').select('*').eq('id', user_id).execute()
     user_list = response.data  # データをuser_list変数に格納
     if not user_list:
@@ -40,17 +43,32 @@ def get_user_data(user_id):
     user_data = {'xp': user['xp'], 'level': user['level']}
     return user_data
 
+
 # 更新
 def update_user_data(user_id, xp, level):
+    user_id = str(user_id)
+    xp = int(float(xp))
+    level = int(float(level))
     response = supabase.table('USER_DATA').update({'xp': xp, 'level': level}).eq('id', user_id).execute()
     return response.data
 
+
 # 挿入
 def insert_user_data(user_id, xp, level):
+    user_id = str(user_id)
+    xp = int(float(xp))
+    level = int(float(level))
     response = supabase.table('USER_DATA').insert({'id': user_id, 'xp': xp, 'level': level}).execute()
     return response.data
+
 
 # ランキング取得
 def get_users_sorted_by_xp():
     response = supabase.table('USER_DATA').select('*').order('xp', desc=True).execute()
-    return response.data
+    users = response.data  # データをusers変数に格納
+    # id をキーにして XP と LEVEL を持つ辞書を作成
+    user_dict = {}
+    for user in users:
+        user_dict[int(user['id'])] = {'xp': user['xp'], 'level': user['level']}
+    # 辞書形式でデータを返却
+    return user_dict
